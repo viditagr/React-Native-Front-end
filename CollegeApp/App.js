9d645b0214beb2448c621eb45b62955b3e7ffe47
Vidit Agrawal <vidit.agrawal@wisc.edu>
2022-07-19 15:56:45 +0530

    App page

--- CollegeApp/App.js
+++ CollegeApp/App.js
@@ -1,12 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Image, View, TextInput, onChangeText, Button, SafeAreaView, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
 
 export default function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+
+  }
+
+  const handleforgotpwd = () => {
+
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <View>
+      <Image style={styles.image} source={require("./assets/log.jpeg")} />
       <StatusBar style="auto" />
-    </View>
+      <TextInput 
+      style={styles.TextInput}
+      placeholder = "Email"
+      />
+      <TextInput
+      style={styles.TextInput}
+      secureTextEntry={true}
+      placeholder = "Password"
+      />
+      </View>
+      <View>
+      <TouchableOpacity
+      style={styles.login_btn}
+      onPress={handleLogin}
+      >
+      <Text style= {styles.logintext}>Login</Text>
+      </TouchableOpacity>
+      </View>
+      <View>
+      <TouchableOpacity
+      style={styles.forgot_btn}
+      onPress={handleforgotpwd}
+      >
+      <Text style= {styles.forgettext}>Forgot Password?</Text>
+      </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -16,5 +55,44 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  image: {
+    width: 150,
+    height: 150,
+    alignSelf: 'center',
+  },
+  TextInput: {
+    width: 200,
+    height: 44,
+    padding: 5,
+    margin: 5,
+  },
+  login_btn: {
+    width: 90,
+    height: 44,
+    alignItems: 'center',
+    borderWidth: 1,
+    borderRadius: 25,
+    justifyContent: 'center',
+    marginBottom: 10,
+  },
+  forgot_btn: {
+    width: 200,
+    height: 44,
+    padding: 5,
+    margin: 5,
+    color: '#fff',
+    borderColor: 'black',
+    borderRadius: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logintext: {
+    color: 'black',
+    fontSize: 15,
+  },
+  forgettext: {
+    color: 'black',
+    fontSize: 12,
   },
 });
