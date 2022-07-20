de56a08ed424b43465c56fba96dcaf40a72eff8e
Shrey-Modi <shreymodi18@gmail.com>
2022-07-20 16:32:29 +0530

    fixed style errors

--- CollegeApp/Screens/Home.js
+++ CollegeApp/Screens/Home.js
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import { StyleSheet, Text, View, Button, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import style from '../Styles/HomeStyle';
+import HomeStyle from '../Styles/HomeStyle';
 
 
 
 export default function HomeScreen({navigation}) {
     return (
+      <View style={HomeStyle.container}>
       <Text>Home Screen</Text>
+      </View>
     );
   }
--- CollegeApp/Styles/HomeStyle.js
+++ CollegeApp/Styles/HomeStyle.js
@@ -1,14 +1,5 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
-  container: {
-    padding: 10,
-    alignItems:'center',
-    justifyContent:'center',
-    backgroundColor: '#43a1c9',
-  },
-  buttonText: {
-    fontSize: 20,
-    textAlign: 'center'
-  }
+    container: { flex: 1, alignItems: 'center', justifyContent: 'center' },
 });
